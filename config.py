8f600ee0043d0a2121873c1f303409210a5797da
BEAST DEVELOPER <beastdeveloper01@gmail.com>
2026-04-05 15:34:07 +0530

    Update Telegram bot configuration values
    Updated bot token, API ID, and API hash with new values.

--- config.py
+++ config.py
@@ -18,9 +18,9 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 #--------------------------------------------
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339:")
-APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8730309806:AAG8IZ5ueZlo7f9b8JyTEjwG62NLxP6jJlo")
+APP_ID = int(os.environ.get("APP_ID", "33461228")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "52850b8b740b1f87bb55329cce4d65a0") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388")) #Your db channel Id
